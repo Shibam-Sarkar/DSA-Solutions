263bb6c7b96f594dde4321ba80cc4df8ff60a92e
SHIBAM SARKAR <sarkarshibam05@gmail.com>
2026-06-06 20:39:56 +0530

    find maximum product of three numbers using sort

--- arrays/Maximum Product of Three Numbers.cpp
+++ arrays/Maximum Product of Three Numbers.cpp
@@ -1,18 +1,12 @@
-int maximumProduct(vector<int>& nums) {
-    int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
-    int min1 = INT_MAX, min2 = INT_MAX;
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
 
-    for (int x : nums) {
-        if (x >= max1)      { max3 = max2; max2 = max1; max1 = x; }
-        else if (x >= max2) { max3 = max2; max2 = x; }
-        else if (x > max3)  { max3 = x; }
+        int n = nums.size();
+        int case1 = nums[n-1] * nums[n-2] * nums[n-3];
+        int case2 = nums[0]   * nums[1]   * nums[n-1];
 
-        if (x <= min1)      { min2 = min1; min1 = x; }
-        else if (x < min2)  { min2 = x; }
+        return max(case1, case2);
     }
-
-    int case1 = max1 * max2 * max3;
-    int case2 = min1 * min2 * max1;
-
-    return max(case1, case2);
-}
+};
